8891e6aba6950664d9f7320f25e577d531cf574c
CRBroughton <crbroughton@posteo.uk>
2025-11-19 20:50:02 +0000

    fix the cursor scaling issues for vscode (related to wayland decorations somehow)

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -5,4 +5,16 @@
   programs.vscode = {
     enable = true;
   };
+
+  # Fix cursor size on Wayland with scaling
+  xdg.desktopEntries.code = {
+    name = "Visual Studio Code";
+    genericName = "Text Editor";
+    exec = "code --enable-features=WaylandWindowDecorations --ozone-platform=wayland %F";
+    terminal = false;
+    categories = [ "Utility" "TextEditor" "Development" "IDE" ];
+    mimeType = [ "text/plain" "inode/directory" ];
+    startupNotify = true;
+    icon = "vscode";
+  };
 }

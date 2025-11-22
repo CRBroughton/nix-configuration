039e4d1323b2331bb4d6a25915448486dec45e0a
CRBroughton <crbroughton@posteo.uk>
2025-11-22 09:15:48 +0000

    add evdev rules to register custom keyboard layouts

--- modules/keyboard.nix
+++ modules/keyboard.nix
@@ -6,6 +6,72 @@
   home.file.".config/xkb/symbols/gallium_v2".source = ../xkb/gallium_v2;
   home.file.".config/xkb/symbols/graphite".source = ../xkb/graphite;
 
+  # XKB rules file to register custom layouts with GNOME
+  home.file.".config/xkb/rules/evdev.xml".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE xkbConfigRegistry SYSTEM "xkb.dtd">
+    <xkbConfigRegistry version="1.1">
+      <layoutList>
+        <layout>
+          <configItem>
+            <name>shavian</name>
+            <shortDescription>Shavian</shortDescription>
+            <description>English (Shavian)</description>
+            <languageList>
+              <iso639Id>eng</iso639Id>
+            </languageList>
+          </configItem>
+          <variantList>
+            <variant>
+              <configItem>
+                <name>iykury</name>
+                <description>English (Shavian, Iykury)</description>
+              </configItem>
+            </variant>
+            <variant>
+              <configItem>
+                <name>igc</name>
+                <description>English (Shavian, Imperial)</description>
+              </configItem>
+            </variant>
+            <variant>
+              <configItem>
+                <name>qwerty</name>
+                <description>English (Shavian, QWERTY)</description>
+              </configItem>
+            </variant>
+            <variant>
+              <configItem>
+                <name>qwerty_inverted</name>
+                <description>English (QWERTY, Shavian on AltGr)</description>
+              </configItem>
+            </variant>
+          </variantList>
+        </layout>
+        <layout>
+          <configItem>
+            <name>gallium_v2</name>
+            <shortDescription>Gallium</shortDescription>
+            <description>English (Gallium)</description>
+            <languageList>
+              <iso639Id>eng</iso639Id>
+            </languageList>
+          </configItem>
+        </layout>
+        <layout>
+          <configItem>
+            <name>graphite</name>
+            <shortDescription>Graphite</shortDescription>
+            <description>English (Graphite)</description>
+            <languageList>
+              <iso639Id>eng</iso639Id>
+            </languageList>
+          </configItem>
+        </layout>
+      </layoutList>
+    </xkbConfigRegistry>
+  '';
+
   # dconf settings to register keyboard layouts in GNOME
   dconf.settings = {
     "org/gnome/desktop/input-sources" = {

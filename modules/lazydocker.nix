828037c6de517beeea4657243c4951d9325e54b4
CRBroughton <crbroughton@posteo.uk>
2025-12-07 15:00:00 +0000

    try change to podman configuration for cachyos

--- modules/lazydocker.nix
+++ modules/lazydocker.nix
@@ -16,25 +16,10 @@
       dockerCompose: podman-compose
   '';
 
-  # Set up environment variable for Docker compatibility with Podman
-  home.sessionVariables = {
-    DOCKER_HOST = "unix:///run/user/$UID/podman/podman.sock";
-  };
-
-  # Activation script to enable and start Podman socket
-  home.activation.enablePodmanSocket = {
-    after = [ "linkGeneration" ];
-    before = [ "reloadSystemd" ];
-    data = ''
-      # Enable the system-provided podman.socket
-      if ! /usr/bin/systemctl --user is-enabled podman.socket >/dev/null 2>&1; then
-        $DRY_RUN_CMD /usr/bin/systemctl --user enable podman.socket
-      fi
-
-      # Start the socket if it's not active
-      if ! /usr/bin/systemctl --user is-active podman.socket >/dev/null 2>&1; then
-        $DRY_RUN_CMD /usr/bin/systemctl --user start podman.socket
-      fi
-    '';
-  };
+  # Podman container registries configuration
+  # This allows using short names like "hello-world" instead of "docker.io/library/hello-world"
+  home.file.".config/containers/registries.conf".text = ''
+    unqualified-search-registries = ["docker.io"]
+    short-name-mode = "enforcing"
+  '';
 }

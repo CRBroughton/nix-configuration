2d0f5912652bb2099fce050d27ac7f53f4b9f2d6
CRBroughton <crbroughton@posteo.uk>
2026-01-19 21:15:57 +0000

    add neo-tree to neovim

--- neovim-config/init.lua
+++ neovim-config/init.lua
@@ -13,4 +13,15 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
-require('lazy').setup({})
+require('lazy').setup({
+    {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  }
+})

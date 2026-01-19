bef52c293efdd9ea96d49a16151700414f367437
CRBroughton <crbroughton@posteo.uk>
2026-01-19 21:05:04 +0000

    add options file

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -8,4 +8,9 @@
   home.file.".config/nvim/init.lua" = {
     source = ../neovim-config/init.lua;
   };
+
+  home.file.".config/nvim/lua" = {
+    source = ../neovim-config/lua;
+    recursive = true;
+  };
 }

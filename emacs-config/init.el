d6b306bd436a3f8dd9140e3a36107d2d14953fd6
CRBroughton <crbroughton@posteo.uk>
2026-01-31 15:37:27 +0000

    add rainbow delimiters, ivy-rich and which-key

--- emacs-config/init.el
+++ emacs-config/init.el
@@ -125,3 +125,16 @@
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))

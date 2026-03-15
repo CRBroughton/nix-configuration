074d4d07f2405680f848649e76b024666b1f8587
CRBroughton <crbroughton@posteo.uk>
2026-03-15 08:38:23 +0000

    sort gnome desktop entries and folders

--- users/craig/gnome.nix
+++ users/craig/gnome.nix
@@ -35,6 +35,117 @@
         #"paperwm@paperwm.github.com"
       ];
     };
+    "org/gnome/desktop/app-folders" = {
+      folder-children = [
+        "System"
+        "Utilities"
+        "Media"
+        "Office"
+        "Games"
+        "Social"
+        "Dev"
+      ];
+    };
+    "org/gnome/desktop/app-folders/folders/System" = {
+      name = "System";
+      apps = [
+        "org.gnome.DiskUtility.desktop"
+        "org.gnome.SystemMonitor.desktop"
+        "org.gnome.Logs.desktop"
+        "org.gnome.Software.desktop"
+        "org.gnome.seahorse.Application.desktop"
+        "org.gnome.Connections.desktop"
+        "org.gnome.baobab.desktop"
+        "org.gnome.Console.desktop"
+        "org.gnome.Settings.desktop"
+        "org.gnome.Extensions.desktop"
+        "com.mattjakeman.ExtensionManager.desktop"
+        "com.github.tchx84.Flatseal.desktop"
+        "io.github.flattool.Warehouse.desktop"
+        "it.mijorus.gearlever.desktop"
+        "nixos-manual.desktop"
+        "com.bitwarden.desktop.desktop"
+        "btop.desktop"
+        "org.kde.kleopatra.desktop"
+        "org.rncbc.qpwgraph.desktop"
+        "org.kde.kwatchgnupg.desktop"
+      ];
+    };
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      name = "Utilities";
+      apps = [
+        "org.gnome.Calculator.desktop"
+        "org.gnome.Characters.desktop"
+        "org.gnome.clocks.desktop"
+        "org.gnome.font-viewer.desktop"
+        "simple-scan.desktop"
+        "yelp.desktop"
+        "org.gnome.Epiphany.desktop"
+        "com.brave.Browser.desktop"
+        "com.transmissionbt.Transmission.desktop"
+        "io.gitlab.news_flash.NewsFlash.desktop"
+        "gscriptor.desktop"
+      ];
+    };
+    "org/gnome/desktop/app-folders/folders/Media" = {
+      name = "Media";
+      apps = [
+        "org.gnome.Music.desktop"
+        "org.gnome.Showtime.desktop"
+        "org.gnome.Loupe.desktop"
+        "org.gnome.Decibels.desktop"
+        "org.gnome.Snapshot.desktop"
+        "com.github.neithern.g4music.desktop"
+        "org.musicbrainz.Picard.desktop"
+      ];
+    };
+    "org/gnome/desktop/app-folders/folders/Office" = {
+      name = "Office";
+      apps = [
+        "org.gnome.Calendar.desktop"
+        "org.gnome.Contacts.desktop"
+        "org.gnome.Maps.desktop"
+        "org.gnome.Weather.desktop"
+        "org.gnome.TextEditor.desktop"
+        "org.gnome.Papers.desktop"
+        "md.obsidian.Obsidian.desktop"
+      ];
+    };
+    "org/gnome/desktop/app-folders/folders/Games" = {
+      name = "Games";
+      apps = [
+        "steam.desktop"
+        "net.lutris.Lutris.desktop"
+        "com.heroicgameslauncher.hgl.desktop"
+        "net.runelite.RuneLite.desktop"
+        "Caves of Qud.desktop"
+        "Dungeon Defenders.desktop"
+        "Mewgenics.desktop"
+        "Slay the Spire.desktop"
+      ];
+    };
+    "org/gnome/desktop/app-folders/folders/Social" = {
+      name = "Social";
+      apps = [
+        "org.gajim.Gajim.desktop"
+        "org.equicord.equibop.desktop"
+        "info.mumble.Mumble.desktop"
+      ];
+    };
+    "org/gnome/desktop/app-folders/folders/Dev" = {
+      name = "Dev";
+      apps = [
+        "code.desktop"
+        "dev.zed.Zed.desktop"
+        "com.mitchellh.ghostty.desktop"
+        "nvim.desktop"
+        "virt-manager.desktop"
+        "micro.desktop"
+        "gvim.desktop"
+        "vim.desktop"
+        "xterm.desktop"
+      ];
+    };
     "org/gnome/desktop/input-sources" = {
       sources = [
         (lib.hm.gvariant.mkTuple [

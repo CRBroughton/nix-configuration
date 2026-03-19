37ed44f560b0d2fea4e79012e063c1fe0d0420b0
CRBroughton <crbroughton@posteo.uk>
2026-03-19 21:07:15 +0000

    add placeholder hardware.nix for mums pc

--- users/mum/hosts/mum-pc/hardware.nix
+++ users/mum/hosts/mum-pc/hardware.nix
@@ -1,4 +1,11 @@
-# Hardware configuration for mum-pc
-# Generate with: nixos-generate-config --show-hardware-config
-# on the actual hardware, then replace this file
-{ }
+# Hardware configuration - replace this with your generated hardware config
+# Generate with: nixos-generate-config --root /mnt --show-hardware-config > hardware.nix
+_:
+
+{
+  # Placeholder root filesystem - replace with output of nixos-generate-config
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+}

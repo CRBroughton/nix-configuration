ed9eea2e952282417981ff13064e591c80b4f2e5
CRBroughton <crbroughton@posteo.uk>
2026-03-20 20:43:18 +0000

    add nix path to the profile file

--- services/code-server/nix-path.sh
+++ services/code-server/nix-path.sh
@@ -1,3 +1,4 @@
 export PATH="/nix-sw/bin:/nix/var/nix/profiles/default/bin:$PATH"
 export NIX_REMOTE=daemon
 export NIX_CONFIG="experimental-features = nix-command flakes"
+export NIX_PATH="nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixpkgs:/nix/var/nix/profiles/per-user/root/channels"

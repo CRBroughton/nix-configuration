329655d9784c124438cf4a234c25cd3c69b459e0
CRBroughton <crbroughton@posteo.uk>
2026-05-06 19:25:38 +0100

    add wayvnc service to mum-pc

--- hosts/mum-pc/default.nix
+++ hosts/mum-pc/default.nix
@@ -10,6 +10,20 @@
   networking.networkmanager.enable = true;
   security.sudo.wheelNeedsPassword = false;
 
+  services.wayvnc = {
+    enable = true;
+    host = "0.0.0.0";
+    port = 5900;
+    extraConfig = ''
+      enable_auth=false
+    '';
+  };
+
+  # Allow wayvnc only from Tailscale CGNAT range
+  networking.firewall.extraInputRules = ''
+    ip saddr 100.64.0.0/10 tcp dport 5900 accept
+  '';
+
   modules.ssh.enable = true;
   modules.gnome.enable = true;
   modules.flatpak.enable = true;
